fd38cae1444325b9fc7428a3c05e81c6caad4f1c
Vinicius Imai <vinicius.imai@gmail.com>
2020-04-23 22:26:48 -0300

    Adicionando configuração para debug no vscode

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express from "express";
-import routes from "./routes/index";
+import express from 'express';
+import routes from './routes/index';
 
 const app = express();
+
+app.use(express.json());
 
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log("🔝 Server Started on port 3333");
+  console.log('🔝 Server Started on port 3333');
 });
