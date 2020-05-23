77af84c5b141c0272966b790338026bd1505759c
Vinicius Imai <vinicius.imai@gmail.com>
2020-05-23 18:29:36 -0300

    Configurando JEST e criando primeiro testes do Create Appointments Service

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,26 @@
+import FakeAppointmentRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
+
+  it('should not be able to create two appointment on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  test('aaa', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
