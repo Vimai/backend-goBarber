12c847800cb86d9ed5a10c18d1c63949aec3b593
Vinicius Imai <vinicius.imai@gmail.com>
2020-05-23 22:30:43 -0300

    Teste para usuário criação de usuário e user repetido

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,34 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import CreateUserService from '@modules/users/services/CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUsers = new CreateUserService(fakeUsersRepository);
+    const user = await createUsers.execute({
+      name: 'vinicius',
+      email: 'vinicius@gmail.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUsers = new CreateUserService(fakeUsersRepository);
+    await createUsers.execute({
+      name: 'vinicius',
+      email: 'vinicius@gmail.com',
+      password: '123456',
+    });
+    expect(
+      createUsers.execute({
+        name: 'vinicius',
+        email: 'vinicius@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
