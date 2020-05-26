85da1136df496dc2bf9543541f04ee8f91fe5e84
Vinicius Imai <vinicius.imai@gmail.com>
2020-05-26 20:33:33 -0300

    Implementando reset de senha

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,46 @@
+import { injectable, inject } from 'tsyringe';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+// import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ token, password }: IRequest): Promise<string> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exist');
+    }
+
+    const user = await this.usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User token does not exist');
+    }
+
+    user.password = password;
+
+    await this.usersRepository.save(user);
+
+    return token + password;
+  }
+}
+
+export default ResetPasswordService;
