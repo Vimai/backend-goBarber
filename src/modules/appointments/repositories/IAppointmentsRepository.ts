6c7927103ba4612fe394064a1c56b73db92fac7b
Vinicius Imai <vinicius.imai@gmail.com>
2020-05-31 21:26:13 -0300

    Listar os agendamentos disponiveis por hora

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import ICreateAppointmentsDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentsDTO): Promise<Appointment>;
@@ -8,4 +9,8 @@ export default interface IAppointmentsRepository {
   findAllInMonthFromProvider(
     date: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    date: IFindAllInDayFromProviderDTO,
+  ): Promise<Appointment[]>;
+}
 }
