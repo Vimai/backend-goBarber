c4eb28bf2fa69c4d4d3c53b47f3e95644ea79ec7
Vinicius Imai <vinicius.imai@gmail.com>
2020-06-03 21:52:18 -0300

    Criando rotas para listar o agendamento

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,26 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
 
 const ProvidersRouter = Router();
 const providerController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 ProvidersRouter.use(ensureAuthenticated);
 
 ProvidersRouter.get('/', providerController.index);
+
+ProvidersRouter.get(
+  '/:provider_id/month-availability',
+  providerDayAvailabilityController.index,
+);
+
+ProvidersRouter.get(
+  '/:provider_id/day-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default ProvidersRouter;
