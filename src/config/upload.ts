550e1744cf7a751a294ecae5599181c12c3ec16a
Vinicius Imai <vinicius.imai@gmail.com>
2020-06-08 19:41:05 -0300

    Alterando para chavear entre o disk e o S3, realizando o delete no s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,42 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
-import { request } from 'express';
+import multer, { StorageEngine } from 'multer';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  drive: 's3' | 'disk';
+
+  tempFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tempFolder,
   uploadsFolder: path.resolve(tempFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
-    },
-  }),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+  },
 };
