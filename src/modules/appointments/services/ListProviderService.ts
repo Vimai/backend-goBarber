295aa11a3c177c978c421da2b5359b59349be418
Vinicius Imai <vinicius.imai@gmail.com>
2020-06-08 21:40:52 -0300

    Usando o cache provider no listProvider

--- src/modules/appointments/services/ListProviderService.ts
+++ src/modules/appointments/services/ListProviderService.ts
@@ -1,9 +1,11 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -14,15 +16,28 @@ class ListProviderService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      exept_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
     if (!users) {
-      throw new AppError('User not found');
+      users = await this.usersRepository.findAllProviders({
+        exept_user_id: user_id,
+      });
+
+      if (!users) {
+        throw new AppError('User not found');
+      }
+
+      console.log('A query no banco foi feita');
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
 
     return users;
