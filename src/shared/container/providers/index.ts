2c5da908a2cef03a7e146f96c4be5f4f23f50851
Vinicius Imai <vinicius.imai@gmail.com>
2020-06-08 21:24:21 -0300

    Terminando de configurar o redis

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,3 +1,4 @@
 import '@shared/container/providers/StorageProvider';
 import '@shared/container/providers/MailTemplateProvider'; // Mail template tem que ficar primeiro que o mail provider
 import '@shared/container/providers/MailProvider';
+import '@shared/container/providers/CacheProvider';
