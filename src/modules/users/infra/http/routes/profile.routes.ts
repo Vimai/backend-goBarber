73fc6a1d9ed00dd34696669f7ac650aa73326a39
Vinicius Imai <vinicius.imai@gmail.com>
2020-06-27 13:03:11 -0300

    Revomento Router duplicado

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,4 @@
-import { Router, Router } from 'express';
+import { Router } from 'express';
 import { celebrate, Segments, Joi } from 'celebrate';
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
